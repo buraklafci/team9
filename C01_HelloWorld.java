f118e675359ed8917fa4349d2d3a30115133c57f
Cihad Atınç <atinccihad@gmail.com>
2022-08-28 16:28:46 +0300

    cihad

--- C01_HelloWorld.java
+++ C01_HelloWorld.java
@@ -4,5 +4,7 @@ public class C01_HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello java,Hello World");
 
+
+        System.out.println("Cihad gonderdigi");
     }
 }

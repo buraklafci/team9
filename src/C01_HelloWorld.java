7382fbd2e96df6778ceab244b55074e6d87bd15f
cuneytsoylu <110092795+cuneytsoylu@users.noreply.github.com>
2022-08-28 16:35:26 +0300

    cuneyt

--- src/C01_HelloWorld.java
+++ src/C01_HelloWorld.java
@@ -2,5 +2,9 @@ public class C01_HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello java,Hello World");
 
+
+        System.out.println("yeni class açmadan gönderiyorum");
+
+
     }
 }
---
+++ src/cuneyt.java
@@ -0,0 +1,5 @@
+public class cuneyt {
+    public static void main(String[] args) {
+        System.out.println("yeni eklenecejk dosyalar bu branch uzerinden yapilacak");
+    }
+}
